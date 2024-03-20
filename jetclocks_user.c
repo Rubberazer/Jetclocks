f3e0aeffae59016c59618a0ced1b266f726eb79c
Rubberazer <47650457+Rubberazer@users.noreply.github.com>
2024-03-20 02:58:04 +0100

    prototype

--- jetclocks_user.c
+++ jetclocks_user.c
@@ -20,31 +20,30 @@ int main() {
 	printf("Opening /dev/jetclocks not possible\n");
 	return -1;
     }
-
-    /* Enabling clock "spi1" */
     
+     /* Setting up rate to clock "spi1" */
+
     strncpy(clock.clk, "spi1", sizeof(clock.clk));
+    clock.clk_set_rate = 40800000; //Default is 81600000
+
+    ioctl(dev, CLK_SET_RATE, &clock);
+    printf("Setting rate: %lu on clock %s: \n",clock.clk_set_rate, clock.clk);
+    
+    /* Checking current rate on clock spi1 */
+
+    ioctl(dev, CLK_GET_RATE, &clock);
+    printf("Rate on clock %s is %lu\n",clock.clk, clock.clk_rate);
+
+     /* Enabling clock "spi1" */
+    
     ioctl(dev, CLK_ENABLE, &clock);
     printf("Enabling clock %s: \n",clock.clk);
 
     /* Now checking whether the clok is enabled*/
 
-    strncpy(clock.clk, "spi1", sizeof(clock.clk));
     ioctl(dev, CLK_IS_ENABLED, &clock);
     printf("clock %s status(0 disabled, 1 enabled): %d\n",clock.clk, clock.clk_enabled);
-
-     /* Setting up rate to clock "spi1" */
     
-    clock.clk_set_rate = 40800000; //Default is 81600000
-    ioctl(dev, CLK_SET_RATE, &clock);
-    printf("Setting rate: %lu on clock %s: \n",clock.clk_set_rate, clock.clk);
-
-    /* Checking current rate on clock spi1 */
-
-    strncpy(clock.clk, "spi1", sizeof(clock.clk));
-    ioctl(dev, CLK_GET_RATE, &clock);
-    printf("Rate on clock %s is %lu\n",clock.clk, clock.clk_rate);
- 
     close(dev);
     return 0;
 }
